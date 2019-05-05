5e0178bbc37ca2bfc65ce7fef5a4cea3c68cc399
liangjunwei <flashwei123@gmail.com>
2019-05-05 15:52:39 -0700

    create a project

---
+++ TravelPlanner/TravelPlanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TravelPlanner
+//
+//  Created by Junwei Liang on 5/5/19.
+//  Copyright © 2019 Junwei Liang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
